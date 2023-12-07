586d1e150c6ff32c9a2129663bb37d4b39f19b58
Iron man <thomas.sporer68@gmail.com>
2023-12-07 15:16:40 +0100

    a valider pour init, me dire ce que tu en pense pour set

--- configuration.c
+++ configuration.c
@@ -1,3 +1,5 @@
+// THOMAS S
+
 #include <configuration.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -27,6 +29,11 @@ void display_help(char *my_name) {
  * @param the_config is a pointer to the configuration to be initialized
  */
 void init_configuration(configuration_t *the_config) {
+    the_config->is_parallel = 1; // TRUE
+    the_config->is_dry_run = 0; // FALSE
+    the_config->is_verbose = 0; // FALSE
+    the_config->uses_md5 = 1; // TRUE
+    the_config->processes_count = 1; // A DEFINIR LE NOMBRE
 }
 
 /*!
@@ -37,4 +44,27 @@ void init_configuration(configuration_t *the_config) {
  * @return -1 if configuration cannot succeed, 0 when ok
  */
 int set_configuration(configuration_t *the_config, int argc, char *argv[]) {
+    if (argc > 8 || argc < 3) { // Teste si le bon nombre d'arguments sont passés en paramètres
+        printf("ERREUR: nombre d'arguments incorrects (%d)\n", argc);
+        return -1;
+    } else {
+        if (argc != 3) {
+            for (int i = 3; i <= argc; ++i) { // Parcours les autres arguments pour les options
+                if (strcmp(argv[i], "--date-size-only") == 0) {
+                    the_config->uses_md5 = 0; // On utilise pas la somme md5
+                } else if (strcmp(argv[i], "--no-parallel") == 0) {
+                    the_config->is_parallel = 0; // On utilise un seul processus pour tout le programme
+                } else if (strcmp(argv[i], "-v") == 0) {
+                    the_config->is_verbose = 1; // On affiche ce que le programme fait
+                } else if (strcmp(argv[i], "--dry-run") == 0) {
+                    the_config->is_dry_run = 1; // On ne copie pas les fichiers
+                } else {
+                    // cas argv[i] = "-n <nombre>"
+                    return -1; // Un des arguments n'est pas correct
+                }
+            }
+        } else {
+            return 0; // Pas de problèmes rencontrés
+        }
+    }
 }
