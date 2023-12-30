8a39c54483f1df9c19c212143842b49a162edc1c
Iron man <thomas.sporer68@gmail.com>
2023-12-30 15:03:18 +0100

    Modifications apportées suite au feedback

--- files-list.c
+++ files-list.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 
+
 /*!
  * @brief clear_files_list clears a files list
  * @param list is a pointer to the list to be cleared
@@ -18,6 +19,7 @@ void clear_files_list(files_list_t *list) {
     }
 }
 
+
 /*!
  *  @brief add_file_entry adds a new file to the files list.
  *  It adds the file in an ordered manner (strcmp) and fills its properties
@@ -28,39 +30,52 @@ void clear_files_list(files_list_t *list) {
  *  @return a pointer to the added element if success, NULL else (out of memory)
  */
 files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
+    if (list == NULL || file_path == NULL) { // Test de la validité des paramètres
+        return NULL; // Paramètres passés invalides
+    }
+
+    files_list_entry_t *existing_entry = list->head; // Variable temporaire pour le test d'existence du fichier dans la liste
+    while (existing_entry != NULL) {
+        if (strcmp(existing_entry->path_and_name, file_path) == 0) { // Vérifie si les deux paramètres sont identiques
+            return NULL; // Le fichier existe déja, donc on ne fait rien
+        }
+        existing_entry = existing_entry->next; // Incrémentation de la variable de parcours
+    }
+
     files_list_entry_t *new_entry = malloc(sizeof(files_list_entry_t)); // Créer une nouvelle entrée temporaire (parcours de la liste)
     if (new_entry == NULL) {
         return NULL; // Erreur d'allocation donc sortie de fonction
-    } else {
-        files_list_entry_t *temp = list->head; //
-        while (temp != NULL && strcmp(temp->path_and_name, file_path) <= 0) { // Cherche la position correcte dans la liste
-            temp = temp->next; // Incrémentation
-        }
-
-        if (temp == NULL) { // Fin de la liste atteinte
-            new_entry->prev = list->tail; // Car dernier élément
-            new_entry->next = NULL; // Pas de suivante car fin de liste
-            if (list->head == NULL) { // Verifie si liste vide (ajouter en tête)
-                list->head = new_entry; // Ajouter en tête
-            } else if (list->tail != NULL) { // Si la queue de liste n'est pas vide
-                list->tail->next = new_entry; // Ajouter en queue
-            } else {
-                list->tail = new_entry; // Si la queue de liste est vide
-            }
-
-        } else { // Ajouter entre deux éléments
-            new_entry->prev = temp->prev; // gestion des listes
-            new_entry->next = temp;
-            if (temp->prev != NULL) {
-                temp->prev->next = new_entry;
-            } else {
-                list->head = new_entry; // Ajouter en tête si pas de précédent
-            }
-            temp->prev = new_entry;
-        }
     }
+
+    files_list_entry_t *temp = list->head; //
+    while (temp != NULL && strcmp(temp->path_and_name, file_path) <= 0) { // Cherche la position correcte dans la liste
+        temp = temp->next; // Incrémentation
+    }
+
+    if (temp == NULL) { // Fin de la liste atteinte
+        new_entry->prev = list->tail; // Car dernier élément
+        new_entry->next = NULL; // Pas de suivante car fin de liste
+        if (list->head == NULL) { // Verifie si liste vide (ajouter en tête)
+            list->head = new_entry; // Ajouter en tête
+        } else if (list->tail != NULL) { // Si la queue de liste n'est pas vide
+            list->tail->next = new_entry; // Ajouter en queue
+        } else {
+            list->tail = new_entry; // Si la queue de liste est vide
+        }
+    } else { // Ajouter entre deux éléments
+        new_entry->prev = temp->prev; // gestion des listes
+        new_entry->next = temp;
+        if (temp->prev != NULL) {
+            temp->prev->next = new_entry;
+        } else {
+            list->head = new_entry; // Ajouter en tête si pas de précédent
+        }
+        temp->prev = new_entry;
+    }
+
     return new_entry; // operation réussie
 }
+
 
 /*!
  * @brief add_entry_to_tail adds an entry directly to the tail of the list
@@ -73,18 +88,19 @@ files_list_entry_t *add_file_entry(files_list_t *list, char *file_path) {
 int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
     if (list == NULL || entry == NULL) { // Verifie validité des arguments
         return -1; // Si paramètres invalides
-    } else {
-        if (list->tail == NULL) { // Pas d'éléments assignés en queue
-            list->tail = entry; // Ajout de l'élément en queue
-        } else {
-            entry->prev = list->tail; // Ajout du chemin vers le précédent
-            list->tail = entry; // Ajout du chemin vers la queue
-            list->tail->next = NULL; // Pas de prochain éléments
-        }
-
-        return 0; // Opération réussie
     }
+
+    if (list->head == NULL) { // La liste est vide
+        list->head = entry; // Ajoute de l'élément en tête de liste car liste vide
+        list->tail = entry; // Ajout de l'élément en queue
+    } else {
+        entry->prev = list->tail; // Ajout du chemin vers le précédent
+        list->tail->next = entry; // L'élément en queue actuelle pointe sur la nouvelle queue de liste
+        list->tail = entry; // Ajout du chemin vers la "nouvelle" queue
+    }
+    return 0; // Opération réussie
 }
+
 
 /*!
  *  @brief find_entry_by_name looks up for a file in a list
@@ -97,18 +113,35 @@ int add_entry_to_tail(files_list_t *list, files_list_entry_t *entry) {
  */
 files_list_entry_t *find_entry_by_name(files_list_t *list, char *file_path, size_t start_of_src, size_t start_of_dest) {
     if (list == NULL || file_path == NULL) { // Vérifie la validité des paramètres
-        return NULL; // Si les paramètres sont invalides
+        return NULL; // Paramètres invalides
     }
-    size_t file_path_length = strlen(file_path); // Longueur du chemin d'accès
-    files_list_entry_t *temp = list->head; // Element temporaire pour le parcours
-    while (temp != NULL) { // Parcours de la liste
-        if (strncmp(temp->path_and_name + start_of_src, file_path + start_of_dest, file_path_length - start_of_dest) == 0) { // Compare les noms des chemins
-            return temp; // Entrée trouvée
+
+    size_t file_path_length = strlen(file_path); // Longueur du chemin du fichier cherché
+    files_list_entry_t *temp = list->head; // Variable pour parcourir la liste, va être incrémenté par la suite
+
+    while (temp != NULL) { // Tant qu'on a pas atteind la fin de la liste (tail->next pointe vers NULL)
+        size_t i = start_of_src; // Deux variables pour parcourir les chemins du fichier cherché et de ceux de la liste
+        size_t j = start_of_dest;
+
+        while (i < sizeof(temp->path_and_name) && j < file_path_length) {
+            char list_char = temp->path_and_name[i+1]; // incrémentation des caractères des chemin comparés
+            char search_char = file_path[j+1];
+
+            if (list_char != search_char) { // Le caractère du chemin d'un fichier de la liste est différent de celui du chemin du fichier cherché
+                break; // On sort de la boucle
+            }
         }
-        temp = temp->next; // Si la comparaison retourne un résultat pas concluant, passe à la prochaine itération
+
+        if (i - start_of_src == file_path_length - start_of_dest) { // Vérifie si on a atteind la fin du chemin ou si deux caractères sont différents
+            return temp; // Les éléments correspondent, on retourne un pointeur sur l'élément
+        }
+
+        temp = temp->next; // Si la comparaison retourne un résultat pas concluant, passe au prochain élément de la liste
     }
-    return NULL; // Entrée pas trouvée
+
+    return NULL; // Entrée ne correspond à aucun élément de la liste
 }
+
 
 /*!
  * @brief display_files_list displays a files list
@@ -123,6 +156,7 @@ void display_files_list(files_list_t *list) {
         printf("%s\n", cursor->path_and_name);
     }
 }
+
 
 /*!
  * @brief display_files_list_reversed displays a files list from the end to the beginning
